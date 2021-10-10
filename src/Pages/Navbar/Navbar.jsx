a6032a02116ecf06f1aed3b12c1c707b5b51f236
sachin7695 <62798405+sachin7695@users.noreply.github.com>
2021-10-10 14:23:01 +0530

    Update Navbar.jsx

--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -7,7 +7,11 @@ class Navbar extends Component {
       <nav class="main-nav">
         <ul class="menu">
           <li class="menu__items to-left">
-            <img className="logo-img" src="./../Backkground.jpeg" alt="logo" />
+            <img
+              className="logo-img"
+              src="C:\Users\ASUS\OneDrive\Desktop\Mind-It-main\src\Components\Assets\Backkground.jpeg"
+              alt="logo"
+            />
           </li>
           <li class="menu__item">
             <a class="menu__link" href="#home">
@@ -30,7 +34,7 @@ class Navbar extends Component {
             </a>
           </li>
           <li class="menu__item">
-            <a class="menu__link" href="#talk">
+            <a class="menu__link" href="https://meet.google.com/">
               Talk
             </a>
           </li>
